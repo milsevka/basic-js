76838803393d9ef220b8ae3cfebb2ee2cc6ee554
Milsevka <lusindami99@gmail.com>
2022-10-10 23:11:03 +0300

    feat: add part2

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity == 'string' && Number(sampleActivity) > 0 && sampleActivity !== '' &&  typeof Number(sampleActivity) === 'number')  {
+     result = Math.ceil(Math.log(15 / Number(sampleActivity))/(0.693 / 5730))
+    if (result < 0) {
+      return false;
+    }
+  } else {
+    return false
+}
+ return result
+   
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,12 +11,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  // const winter = [12, 1, 2];
-  // const spring = [3, 4, 5];
-  // const summer = [6, 7, 8];
-  // const autumn = [9, 10, 11];
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+if (!date) {
+  return 'Unable to determine the time of year!'
+}
+if (Object.prototype.toString.call(date) !== '[object Date]' || Object.getOwnPropertyNames(date).length > 0) {
+  throw new Error('Invalid date!');
+}
+const season = {
+  winter: [11, 0, 1],
+  spring: [2, 3, 4],
+  summer: [5, 6, 7],
+  autumn: [8, 9, 10]
+}
+let month = date.getMonth()
+let i = 0;
+for (let key in season) {
+  if (season[key].includes(month)) {
+         return Object.keys(season)[i];
+      }
+      
+  i=i+1;
+}
+
 }
 
 module.exports = {
