f93926a4a824dac3217f17f00f369521d6a5dd38
Milsevka <lusindami99@gmail.com>
2022-10-14 12:17:09 +0300

    feat: add repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let resultStr = []
+  let miniStr = []
+  if (typeof(str) != 'String') {
+    str = String(str)
+  }
+  
+  let repeat = (options.hasOwnProperty('repeatTimes')) ? options.repeatTimes : 1;
+  let separBig = (options.hasOwnProperty('separator')) ? options.separator : '+';
+  let addition = (options.hasOwnProperty('addition')) ? options.addition : '';
+  let addRepeat = (options.hasOwnProperty('additionRepeatTimes')) ? options.additionRepeatTimes : 1;
+  let addSepar = (options.hasOwnProperty('additionSeparator')) ? options.additionSeparator : '|'; 
+  if (typeof(addition) != 'String') {
+    addition = String(addition)
+  }
+  for (let i = 0; i < addRepeat; i++) {
+   miniStr.push(addition)
+  }
+  let miniStrResult = miniStr.join(`${addSepar}`)
+  if (miniStrResult.length > 0) {
+    for (let j = 0; j < repeat; j++) {
+      resultStr.push(str.concat(miniStrResult))
+     }
+  } else {
+    for (let j = 0; j < repeat; j++) {
+      resultStr.push(str)
+     }
+  }
+
+ return resultStr.join(`${separBig}`)
 }
 
 module.exports = {
