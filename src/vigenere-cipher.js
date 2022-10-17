7ec980358e089d1f43daf333a89558735b05c39d
Milsevka <lusindami99@gmail.com>
2022-10-17 14:15:29 +0300

    feat: add cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,75 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ 
+  constructor(type = true) {
+    this.type = type
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(str, key) {
+    if (!str || !key) {
+      throw new Error("Incorrect arguments!"); 
+    }
+    key = key.toUpperCase()
+    str = str.toUpperCase()
+    let arrStr = []
+    let bigKey =  key.repeat(Math.ceil(str.length / key.length)).toUpperCase()
+   for (let i = 0; i < str.length; i++) {
+    let res = str.charCodeAt(i)
+    if (str.charCodeAt(i) > 64 && str.charCodeAt(i) < 91) {
+      res = bigKey.charCodeAt(i) + str.charCodeAt(i) - 65
+      if (res > 90) {
+        res = res - 26
+      }
+    } else {
+     let temp1 = bigKey.slice(0, i);
+     let temp2 = bigKey.slice(i);
+     temp1 = temp1.concat(str[i])
+     bigKey = temp1.concat(temp2)
+    }
+    arrStr.push(String.fromCharCode(res))
+   }
+   if(this.type){
+    return arrStr.join('')
+   } else {
+    return arrStr.reverse().join('');
+   }
   }
+
+  decrypt(encryptStr, key) {
+    if (!encryptStr || !key) {
+      throw new Error("Incorrect arguments!"); 
+    }
+    key = key.toUpperCase()
+    encryptStr = encryptStr.toUpperCase()
+    let arrEnc = []
+    let bigKey =  key.repeat(Math.ceil(encryptStr.length / key.length)).toUpperCase()
+   for (let i = 0; i < encryptStr.length; i++) {
+    let res = encryptStr.charCodeAt(i)
+    if (encryptStr.charCodeAt(i) > 64 && encryptStr.charCodeAt(i) < 91) {
+      res = encryptStr.charCodeAt(i) - bigKey.charCodeAt(i) + 65
+      if (res < 65) {
+        res = res + 26
+      }
+    } else {
+     let temp1 = bigKey.slice(0, i);
+     let temp2 = bigKey.slice(i);
+    temp1 = temp1.concat(encryptStr[i])
+     bigKey = temp1.concat(temp2)
+    }
+    arrEnc.push(String.fromCharCode(res))
+   }
+   if(this.type){
+    return arrEnc.join('')
+   } else {
+    return arrEnc.reverse().join('');
+   }
+  }
+
 }
 
 module.exports = {
   VigenereCipheringMachine
 };
+ 
+    // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
